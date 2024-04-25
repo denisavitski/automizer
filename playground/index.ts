0c9afec5c4beda2b5ac442076003bf8ff57279b7
denisavitski <denisavitski@gmail.com>
2024-04-25 16:53:11 +0300

    clearDir

--- playground/index.ts
+++ playground/index.ts
@@ -1,16 +1,13 @@
-// import { ImageSequenceGenerator } from '../src'
-// import { BackgroundRemoval } from '../src/generators/BackgroundRemoval'
+import { ImageSequenceGenerator } from '../src'
+import { BackgroundRemoval } from '../src/generators/BackgroundRemoval'
 
-// // console.log('A')
+await new ImageSequenceGenerator({
+  srcFolder: 'playground/source/video/1.mp4',
+  destFolder: 'playground/source/video/seq',
+  fps: 40,
+}).generate()
 
-// await new ImageSequenceGenerator({
-//   srcFolder: 'playground/source/video/1.mp4',
-//   destFolder: 'playground/source/video/seq',
-// }).generate()
-
-// console.log('B')
-
-// await new BackgroundRemoval({
-//   srcFolder: 'playground/source/video/seq',
-//   destFolder: 'playground/source/video/seq-wb',
-// }).generate()
+await new BackgroundRemoval({
+  srcFolder: 'playground/source/video/seq',
+  destFolder: 'playground/source/video/seq-wb',
+}).generate()
--- src/generators/ImageSequenceGenerator.ts
+++ src/generators/ImageSequenceGenerator.ts
@@ -3,6 +3,7 @@ import extractFrames from 'ffmpeg-extract-frames'
 import { mkdirSync } from 'fs'
 
 import { sep } from 'path'
+import { clearDir } from '../utils/clearDir'
 
 export interface ImageSequenceGeneratorParameters {
   srcFolder: string
@@ -21,13 +22,14 @@ export class ImageSequenceGenerator {
   constructor(parameters: ImageSequenceGeneratorParameters) {
     this.#srcFolder = parameters.srcFolder
     this.#destFolder = parameters.destFolder
-    this.#destPath = `${this.#destFolder}${sep}${parameters.name || 'frame'}-%d.${
-      parameters.extension || 'png'
-    }`
+    this.#destPath = `${this.#destFolder}${sep}${
+      parameters.name || 'frame'
+    }-%d.${parameters.extension || 'png'}`
     this.#fps = parameters.fps || 60
   }
 
   public async generate() {
+    clearDir(this.#destFolder)
     mkdirSync(this.#destFolder, { recursive: true })
 
     return extractFrames({
